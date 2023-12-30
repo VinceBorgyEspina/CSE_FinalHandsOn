a430402393f2717d20782500fbc86eac42dd6797
VinceBorgyEspina <202180167@psu.palawan.edu.ph>
2023-12-31 01:29:12 +0800

    ayoooo

--- app.py
+++ app.py
@@ -13,3 +13,26 @@ app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
 print(mysql)
+
+def generate_xml_response(results):
+    xml_data = dicttoxml.dicttoxml(results)
+    return Response(xml_data, mimetype='text/xml')
+
+def data_fetch(query):
+    try:
+        format_requested = request.args.get('format') 
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        
+        results = cur.fetchall()
+        
+        if format_requested and format_requested.lower() == 'xml':
+            xml_data = generate_xml_response(results)
+            return xml_data
+        else:
+            return jsonify(results)
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        cur.close()
